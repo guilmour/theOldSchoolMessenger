2a89f24b5c0c69c4788768d0b9d5cfc6f48f916f
Guilmour <guilmour@guilst.com>
2015-03-02 03:22:50 -0300

    cade o create server?

---
+++ src/oldschoolmessenger/CreateServer.java
@@ -0,0 +1,24 @@
+package oldschoolmessenger;
+
+import java.io.PrintStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.ServerSocket;
+
+
+
+/**
+ *
+ * @author guilmour
+ */
+public class CreateServer {
+    ServerSocket serverSocket = null;
+    Socket clientSocket = null;
+    int maxConexoes = 10;
+    
+    
+    
+    
+    
+    
+}
